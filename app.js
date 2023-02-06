29202b1ed1067316c42af3fb5943d2d2265b41a0
AbdallahAbada <abdallah.abada.1993@gmail.com>
2023-02-06 16:24:59 +0000

    adding the the digital time section

--- app.js
+++ app.js
@@ -162,6 +162,20 @@ window.addEventListener('load', () => {
     }
 })
 
+// DISPLAY THE DIGITAL CLOCK
+setInterval(showTime, 1000);
+function showTime() {
+    let time = new Date();
+    let hour = time.getHours()
+    let min = time.getMinutes()
+    let am_pm = "Am"
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    let currentTime = hour + ":" + min;
+    let digiClock = document.getElementById("clock");
+    digiClock.textContent = currentTime
+}
+
 
 
 
